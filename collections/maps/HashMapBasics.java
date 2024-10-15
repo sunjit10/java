7bd21aed6698c7ac494886bd2be1125c4b3fcdac
SUNJIT Tara <sunjit@gmail.com>
2024-10-15 15:15:53 -0500

    Initial for maps

---
+++ collections/maps/HashMapBasics.java
@@ -0,0 +1,17 @@
+package collections.maps;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class HashMapBasics {
+    public static void main(String[] args) {
+	Map<String, Object> parameters = new HashMap<>();
+	parameters.put("Hi", "Hi there");
+	parameters.put("Hello", null);
+	parameters.put("Hola", "Hola senora");
+
+
+	
+    }
+}
