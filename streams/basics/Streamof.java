2c824a1eb77ce344c83647231462a25ee6f0d50e
SUNJIT Tara <sunjit@gmail.com>
2025-02-21 08:43:49 -0600

    Minor refactoring

--- streams/basics/Streamof.java
+++ streams/basics/Streamof.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 
 public class Streamof {
     public static void main(String[] args) {
-	Stream<String> stringStream = Stream.of("Atlantic", "Pacific", "Artic", "Indian");
-	stringStream.sorted().forEach(System.out::println);
+	    Stream<String> stringStream = Stream.of("Atlantic", "Pacific", "Artic", "Indian");
+        stringStream.sorted().forEach(System.out::println);
     }
 }
